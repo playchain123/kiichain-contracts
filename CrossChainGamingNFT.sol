70980c921ae06db8c7c33b806d82ced75730f608
Amaan <amaansayyad@yahoo.com>
2024-09-16 15:26:01 +0530

    Update CrossChainGamingNFT.sol

--- CrossChainGamingNFT.sol
+++ CrossChainGamingNFT.sol
@@ -1,1 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+    contract CrossChainGamingNFT is ERC721, Ownable(msg.sender) {
+    uint256 public nextTokenId;
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor() ERC721("CrossChain Gaming NFT", "CGNFT") {}
+
+    function mint(address to, string memory _tokenURI) external onlyOwner {
+        uint256 tokenId = nextTokenId++;
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal {
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return _tokenURIs[tokenId];
+    }
+
+    function transferNFT(
+        address to,
+        uint256 tokenId
+    ) external {
+        require(ownerOf(tokenId) == msg.sender, "You are not the owner of this NFT");
+        _transfer(msg.sender, to, tokenId);
+    }
+}
