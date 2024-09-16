528e009de35347b842c68aba9548f5785275874b
Amaan <amaansayyad@yahoo.com>
2024-09-16 15:23:47 +0530

    Update GameEngagementToken.sol

--- GameEngagementToken.sol
+++ GameEngagementToken.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract GameEngagementToken is ERC20 {
+    address public admin;
+
+    constructor() ERC20("Game Engagement Token", "GET") {
+        admin = msg.sender;
+        _mint(msg.sender, 1000000 * 10 ** decimals()); // Initial supply to admin
+    }
+
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == admin, "Only admin can mint tokens");
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external {
+        require(msg.sender == admin, "Only admin can burn tokens");
+        _burn(from, amount);
+    }
+
+    function transferAdmin(address newAdmin) external {
+        require(msg.sender == admin, "Only admin can transfer admin rights");
+        admin = newAdmin;
+    }
+}
